253cfce0290de8d295dd7c5ac0bf2c387459a794
Nikita Malinkin <nik.malina20@gmail.com>
2024-08-30 18:53:12 +0300

    Initial commit

---
+++ src/test/java/com/malina/springtesting/SpringTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malina.springtesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
